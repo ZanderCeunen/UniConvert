5c4690355d2ad7e3bc22bd155ac613641a81a130
zander-ceunen <ZanderCeunen@protonmail.com>
2023-12-28 11:12:52 +0100

    UI numcheck

--- Cli/UI.py
+++ Cli/UI.py
@@ -1,5 +1,20 @@
 import Core
-#printing ASCII graphics
+
+
+class Numcheck:
+    def picture(number):
+        extensies = ["none", "jpeg", "png", "gif", "heic", "heif", "Webp", "bmp", "tiff", "ico"]
+        return extensies[number]
+
+    def video(number):
+        extensies = []
+        return extensies[number]
+
+    def muziek(number):
+        extensies = []
+        return extensies[number]
+
+# Printing ASCII graphics.
 print("""                                                                                           
                                                                                            
   ,----..                                                       ___                        
@@ -24,8 +39,8 @@ print("""+----------+-----------+-----------+---------------+
 | 1 video  |  2 Muziek |  3 foto's |  4 documenten |
 +----------+-----------+-----------+---------------+""")
 while True:
-    soort_bestand = int(input("Welk type bestand wil je omzetten? Nummer:"))
-    if (5 > soort_bestand > 0):
+    soort_bestand = int(input("Welk type bestand(en) wil je omzetten? Nummer:"))
+    if 5 > soort_bestand > 0:
         break
     else:
         print("Dit is geen geldig nummer van een bestandstype. Probeer opnieuw")
@@ -40,6 +55,22 @@ elif soort_bestand == 3:
 | 2 png  | 5 heif | 8 tiff |
 | 3 gif  | 6 Webp | 9 ico  |
 +--------+--------+--------+""")
+    while True: #zet nummer om in extensie voor de input bestanden.
+        gekozen_input_formaat_nummer = int(input("Welke extensie heeft/hebben het/de invoer bestand(en)? Nummer:"))
+        if 10 > gekozen_input_formaat_nummer > 0:
+            break
+        else:
+            print("Dit is geen geldig nummer van een bestandsextensie. Probeer opnieuw")
+    gekozen_input_formaat_extensie = Numcheck.picture(gekozen_input_formaat_nummer)
+
+    while True: #zet nummer om in extensie voor de uitvoer bestanden.
+        gekozen_output_formaat_nummer = int(input("Welke extensie heeft/hebben het/de uitvoer bestand(en)? Nummer:"))
+        if 10 > gekozen_output_formaat_nummer > 0:
+            break
+        else:
+            print("Dit is geen geldig nummer van een bestandsextensie. Probeer opnieuw")
+    gekozen_output_formaat_extensie = Numcheck.picture(gekozen_output_formaat_nummer)
+
 
 elif soort_bestand == 4:
     print("")
