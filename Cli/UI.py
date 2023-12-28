b4ad043d958505d38a67bd3ce4bd8684032a7626
zander-ceunen <ZanderCeunen@protonmail.com>
2023-12-28 13:17:34 +0100

    Comments for Picture

--- Cli/UI.py
+++ Cli/UI.py
@@ -1,8 +1,10 @@
 import Core
 import os
 
+# Gebruik deze functie om het ingegeven nummer omtezetten in de juiste extensie.
 class Numcheck:
     def picture(number):
+        # Lijst met ondersteunde extensies maar 0 geen waarden geven.
         extensies = ["none", "jpeg", "png", "gif", "heic", "heif", "Webp", "bmp", "tiff", "ico"]
         return extensies[number]
 
@@ -14,6 +16,7 @@ class Numcheck:
         extensies = []
         return extensies[number]
 
+
 def getfilenames():
     print("Hoe wil je de bestanden selecteren?")
     print("""+---------------------------------------------------------------------+
@@ -22,17 +25,36 @@ def getfilenames():
 | 2) Ik geef zelf de bestand(s)naam(en) in                            |
 +---------------------------------------------------------------------+
     """)
+    # Vragen welke selectie methoden ze willen gebruiken.
     while True:
         keuze_bestandsmethode = int(input("Nummer:"))
         if 3 > keuze_bestandsmethode > 0:
             break
         else:
             print("Dit nummer is geen optie uit de lijst. Probeer opnieuw.")
-    if keuze_bestandsmethode == 1:
-         return os.listdir(os.path.dirname(os.path.realpath(__file__)))
 
+    if keuze_bestandsmethode == 1:
+        # Alle bestanden toevoegen aan de lijst van de map waar script nu instaat.
+         lijst_bestanden = os.listdir(os.path.dirname(os.path.realpath(__file__)))
+         return lijst_bestanden
+    elif keuze_bestandsmethode == 2:
+        print("Geef de naam van het bestand(en) als het bestand zich in dezelfde map bevind als de applicatie.")
+        print("Anders kan je het volledige pad geven")
+        while True:
+            #Lijst maken
+            lijst_bestanden = []
+            # Gebruiker laten toevoegen
+            lijst_bestanden += [input("Naam/pad?:")]
+            nog = input("Wil je nog bestanden ingeven Y/N?:")
+            if nog == "Y" or "y" or "J" or "j":
+                continue
+            else:
+                break
+        return lijst_bestanden
 
 # Printing ASCII graphics.
+#Greetings
+
 print("""                                                                                                                                                                               
   ,----..                                                       ___                        
  /   /   \                                                    ,--.'|_                      
@@ -55,6 +77,7 @@ print(""" +-+-+-+-+-+-+-+ +-+ +-+-+-+-+-+-+-+-+-+-+-+-+
 print("""+----------+-----------+-----------+---------------+
 | 1 video  |  2 Muziek |  3 foto's |  4 documenten |
 +----------+-----------+-----------+---------------+""")
+# Welke converter moet worden gebruikt.
 while True:
     soort_bestand = int(input("Welk type bestand(en) wil je omzetten? Nummer:"))
     if 5 > soort_bestand > 0:
@@ -67,12 +90,13 @@ if soort_bestand == 1:
 elif soort_bestand == 2:
     print("")
 elif soort_bestand == 3:
+    # Als foto geef de ondersteunde extensies
     print("""+--------+--------+--------+
 | 1 jpeg | 4 heic | 7 bmp  |
 | 2 png  | 5 heif | 8 tiff |
 | 3 gif  | 6 Webp | 9 ico  |
 +--------+--------+--------+""")
-    while True: #zet nummer om in extensie voor de input bestanden.
+    while True: # Zet nummer om in extensie voor de input bestanden.
         gekozen_input_formaat_nummer = int(input("Welke extensie heeft/hebben het/de invoer bestand(en)? Nummer:"))
         if 10 > gekozen_input_formaat_nummer > 0:
             break
@@ -86,8 +110,11 @@ elif soort_bestand == 3:
             break
         else:
             print("Dit is geen geldig nummer van een bestandsextensie. Probeer opnieuw")
+    # Zet het nummer om in een extensie.
     gekozen_output_formaat_extensie = Numcheck.picture(gekozen_output_formaat_nummer)
+    # Vraag de lijst met bestanden op.
     bestanden = getfilenames()
+    # Voor elk bestand in de lijst bijhorende Core converter uitvoeren.
     for filename in bestanden:
         Core.foto_omzetter(filename, gekozen_input_formaat_extensie, gekozen_output_formaat_extensie)
 
